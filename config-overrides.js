ba7946e7ebf2e24f1404d55ad0d6d5ce44289696
idonghoon <devarchi33@gmail.com>
2020-05-12 12:10:04 +0800

    Add htmlWebpackPlugin config

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,17 @@
+const path = require('path');
+const srcRoot = path.resolve(__dirname, 'public');
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpack = config => {
+    config.plugins.push(new htmlWebpackPlugin({
+        publicPath: "/",
+        scriptLoading: "defer",
+        minify: true,
+        template: path.resolve(srcRoot, 'index.html'),
+    }));
+    return config
+};
+
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
@@ -9,4 +23,5 @@ module.exports = override(
         javascriptEnabled: true,
         // modifyVars: { '@primary-color': '#25b864' },
     }),
+    htmlWebpack
 );
